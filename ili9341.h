5e832e20d2e945560471390835ddff389522de38
GiselleMylHin <167721565+GiselleMylHin@users.noreply.github.com>
2026-06-04 15:12:56 -0700

    Create ili9341.h

---
+++ ili9341.h
@@ -0,0 +1,32 @@
+/*
+ * ili9341.h
+ *
+ *  Created on: May 26, 2026
+ *      Author: Jessica Sierra
+ */
+#ifndef INC_ILI9341_H_
+#define INC_ILI9341_H_
+
+#include "main.h"
+#include <stdint.h>
+
+#define ILI9341_WIDTH   240
+#define ILI9341_HEIGHT  320
+
+#define ILI9341_BLACK   0x0000
+#define ILI9341_WHITE   0xFFFF
+
+#define ILI9341_RED     0xF800
+#define ILI9341_GREEN   0x07E0
+#define ILI9341_BLUE    0x001F
+#define ILI9341_YELLOW  0xFFE0
+
+void ILI9341_Init(void);
+void ILI9341_FillScreen(uint16_t color);
+void ILI9341_FillRect(uint16_t x, uint16_t y,uint16_t w, uint16_t h, uint16_t color);
+void ILI9341_DrawUpArrow(uint16_t color);
+void ILI9341_DrawDownArrow(uint16_t color);
+void ILI9341_DrawLeftArrow(uint16_t color);
+void ILI9341_DrawRightArrow(uint16_t color);
+
+#endif
