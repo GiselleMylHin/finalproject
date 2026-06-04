7c9f93a6be164d8276a149ffef47405a0952e1f3
GiselleMylHin <167721565+GiselleMylHin@users.noreply.github.com>
2026-06-04 15:10:23 -0700

    Create buttons.h

---
+++ buttons.h
@@ -0,0 +1,11 @@
+#ifndef BUTTONS_H
+#define BUTTONS_H
+
+#include "main.h"
+
+void Buttons_Init(void);
+uint8_t Button_StartPressed(void);
+uint8_t Button_SelectPressed(void);
+uint8_t Button_ResetPressed(void);
+
+#endif
