4a41d0c358b105acd2b02b96f5a4e98399b748e3
GiselleMylHin <167721565+GiselleMylHin@users.noreply.github.com>
2026-06-05 01:37:29 -0700

    Update ili9341.h

--- ili9341.h
+++ ili9341.h
@@ -1,9 +1,3 @@
-/*
- * ili9341.h
- *
- *  Created on: May 26, 2026
- *      Author: Jessica Sierra
- */
 #ifndef INC_ILI9341_H_
 #define INC_ILI9341_H_
 
@@ -15,18 +9,24 @@
 
 #define ILI9341_BLACK   0x0000
 #define ILI9341_WHITE   0xFFFF
-
 #define ILI9341_RED     0xF800
 #define ILI9341_GREEN   0x07E0
 #define ILI9341_BLUE    0x001F
 #define ILI9341_YELLOW  0xFFE0
+#define ILI9341_CYAN    0x07FF
+#define ILI9341_MAGENTA 0xF81F
+#define ILI9341_ORANGE  0xFD20
 
 void ILI9341_Init(void);
 void ILI9341_FillScreen(uint16_t color);
-void ILI9341_FillRect(uint16_t x, uint16_t y,uint16_t w, uint16_t h, uint16_t color);
+void ILI9341_FillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
 void ILI9341_DrawUpArrow(uint16_t color);
 void ILI9341_DrawDownArrow(uint16_t color);
 void ILI9341_DrawLeftArrow(uint16_t color);
 void ILI9341_DrawRightArrow(uint16_t color);
+
+/* Text functions */
+void ILI9341_DrawChar(uint16_t x, uint16_t y, char c, uint16_t color, uint16_t bg, uint8_t size);
+void ILI9341_DrawString(uint16_t x, uint16_t y, const char *str, uint16_t color, uint16_t bg, uint8_t size);
 
 #endif
